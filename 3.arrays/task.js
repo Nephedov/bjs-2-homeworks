c5c7ced4f67beb1b51e0f11f1fad05304cd76c90
Mihail Nephedov <Nephedov93@gmail.com>
2023-02-07 22:49:16 +0300

    Task1 Task2 on 3.arrays Completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length) {
+        return arr1.every((value, index) => value === arr2[index]);
+    } 
+    else return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(people => people.gender === gender)
+        .map(ages => ages.age).reduce((ageSum, age) => ageSum + age, 0) / 
+                users.filter(people => people.gender === gender).map(ages => ages.age).length;
+    return result ? result : 0;
 }
