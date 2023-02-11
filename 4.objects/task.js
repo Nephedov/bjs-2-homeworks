dd22fc04ab3f6f28b4cd26091e51f7229b0ae441
Mihail Nephedov <Nephedov93@gmail.com>
2023-02-12 02:52:49 +0300

    Task1 on 4.objects Completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,26 @@
 function Student(name, gender, age) {
-  
+  this.name = name,
+  this.gender = gender,
+  this.age = age,
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.marks) {
+    this.marks = [...this.marks, ...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  return this.marks && this.marks.length > 0 ? this.marks.reduce((marksSum, mark, index, marks) => marksSum + mark / marks.length, 0) : 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
