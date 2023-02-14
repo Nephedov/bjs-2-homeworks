bcf806ff5ed530b20f24576d5785a3173383995d
Mihail Nephedov <Nephedov93@gmail.com>
2023-02-14 22:58:43 +0300

    Task1 Task2 on 6.exception-closure Completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,45 @@
-﻿
+﻿function parseCount(value) {
+    if(Number.isNaN(Number.parseFloat(value))) {
+        throw new Error("Невалидное значение");
+    } else return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch(error){
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(sideOne, sideTwo, sideThree) {
+        if(sideOne + sideTwo < sideThree || sideOne + sideThree < sideTwo || sideTwo + sideThree < sideOne) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+        this.sideOne = sideOne;
+        this.sideTwo = sideTwo;
+        this.sideThree = sideThree;
+        }
+    }
+
+    get perimeter() {
+        return this.sideOne + this.sideTwo + this.sideThree;
+    }
+
+    get area() {
+        const semiPerimeter = (this.perimeter) * 0.5;
+        return +Math.sqrt(semiPerimeter*(semiPerimeter-this.sideOne)*(semiPerimeter-this.sideTwo)*(semiPerimeter-this.sideThree)).toFixed(3);
+    }
+}
+
+function getTriangle(sideOne, sideTwo, sideThree) {
+    try {
+       return new Triangle(sideOne, sideTwo, sideThree);
+    } catch(error) {
+        return {
+            get area() {return "Ошибка! Треугольник не существует"},
+            get perimeter() {return "Ошибка! Треугольник не существует"}
+        }
+    }
+}
