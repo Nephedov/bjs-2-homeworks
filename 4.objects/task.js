0626c331e17dae7c932eac4720eb00ddf07a7c70
Mihail Nephedov <Nephedov93@gmail.com>
2024-04-28 15:05:31 +0300

    modiffied 4.objects/task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,7 +11,7 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marks) {
   if(this.marks) {
-    this.marks = [...this.marks, ...marks];
+    this.marks.push(...marks);
   }
 }
 
